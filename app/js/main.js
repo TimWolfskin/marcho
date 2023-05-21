1d7a208b4c20ee546edc7313f4572616eda9fddd
TimWolfskin <timwolfskin013@gmail.com>
2023-05-21 17:55:35 +0300

    Added product slide and product info in product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,34 @@
 $(function () {
-  $(".shop-content__filter-btn").on("click", function () {
-    $(".shop-content__filter-btn").removeClass('shop-content__filter-btn--active')
-    $(this).addClass('shop-content__filter-btn--active')
+  $(".product-slide__thumb").slick({
+    asNavFor: ".product-slide__big",
+    focusOnSelect: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false,
+  });
+  $(".product-slide__big").slick({
+    asNavFor: ".product-slide__thumb",
+    draggable: false,
+    arrows: false,
   });
 
-  $('.button-list').on('click', function(){
-    $('.product-item').addClass('product-item--list')
-  })
+  $(".shop-content__filter-btn").on("click", function () {
+    $(".shop-content__filter-btn").removeClass(
+      "shop-content__filter-btn--active"
+    );
+    $(this).addClass("shop-content__filter-btn--active");
+  });
 
-  $('.button-grid').on('click', function(){
-    $('.product-item').removeClass('product-item--list')
-  })
+  $(".button-list").on("click", function () {
+    $(".product-item").addClass("product-item--list");
+  });
 
-  $(".select-style").styler();
+  $(".button-grid").on("click", function () {
+    $(".product-item").removeClass("product-item--list");
+  });
+
+  $(".select-style, .product-one__item-num").styler();
 
   $(".filter-price__input").ionRangeSlider({
     type: "double",
